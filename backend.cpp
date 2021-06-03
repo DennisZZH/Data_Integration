936acd536991b9c9c904c9f3dc046acb12bec215
Zihao Zhang <zihaozhang@ucsb.edu>
2021-06-03 16:59:52 -0700

    enable save output to disk

--- backend.cpp
+++ backend.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <vector>
 #include <string>
@@ -12,13 +13,19 @@ using namespace std;
 static int callback(void* data, int argc, char** argv, char** azColName)
 {
     int i;
+    ofstream myfile;
+    myfile.open("output.txt", ios::app);
     fprintf(stderr, "%s:\n", (const char*)data);
   
     for (i = 0; i < argc; i++) {
         printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+        string colName = azColName[i], val = argv[i] ? argv[i] : "NULL";
+        myfile << colName << " = " << val << endl;
     }
-  
-    printf("\n");
+    cout << endl;
+    myfile << endl;
+
+    myfile.close();
     return 0;
 }
 
@@ -34,7 +41,7 @@ int query_db(string sql_query) {
         std::cout << "Opened Database Successfully!" << std::endl;
 
     string data("Results found");
-    string sql("SELECT * FROM MOVIES");
+    //string sql("SELECT * FROM MOVIES");
     int rc = sqlite3_exec(DB, sql_query.c_str(), callback, (void*)data.c_str(), NULL);
   
     if (rc != SQLITE_OK)
