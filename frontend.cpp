ed608df6a46a04b95102bf99b5a0d6b458d9622c
Zihao Zhang <zihaozhang@ucsb.edu>
2021-06-06 18:43:19 -0700

    improved frontend

--- frontend.cpp
+++ frontend.cpp
@@ -12,32 +12,75 @@
 
 using namespace std;
 
-vector<string> listen_and_store(string n){
-	// listen for input
-	string input;
-	getline(cin, input);
-	stringstream ss(input);
-	vector<string> list;
-	if(n!=""){
-		list.push_back(n);
+vector<string> str_split_semicol(string str) {
+	vector<string> res;
+	size_t start = 0, pos = str.find(";");
+	string s;
+	while (pos != string::npos) {
+		s = str.substr(start, pos - 1 - start);
+		res.push_back(s);
+		start = pos + 1;
+		pos = str.find(";", start);
 	}
-
-	// store parsed input to list
-	while(ss.good()){
-		string sub;
-    	getline(ss, sub, ',');
-    	list.push_back(sub);
+	if (start != str.length()) {
+		s = str.substr(start);
+		res.push_back(s);
 	}
-
-	return list;
+	return res;
 }
 
-vector<vector<string> > front_end() {
+/* Flag bit is set if we want to include schema name into the vector*/
+vector<string> str_strip_parens(string str, bool flag) {
+	vector<string> res;
+	string title_str, paras_str;
+	size_t pos_start = str.find("("), pos_end = str.find(")");
+	
+	if (flag) {
+		title_str = str.substr(0, pos_start);
+		res.push_back(title_str);
+	}
+	
+	paras_str = str.substr(pos_start + 1, pos_end - pos_start - 1);
+	size_t start = 0, pos = paras_str.find(",");
+	string s;
+	while (pos != string::npos) {
+		s = paras_str.substr(start, pos - start);
+		res.push_back(s);
+		start = pos + 1;
+		pos = paras_str.find(",", start);
+	}
+	if (start != paras_str.length()) {
+		s = paras_str.substr(start);
+		res.push_back(s);
+	}
 
-	vector<vector<string> > global_conj_queries;
+	return res;
+}
+
+vector<string> get_answer_selection(string conj_str) {
+	size_t pos = conj_str.find("<-");
+	string ans_str = conj_str.substr(0, pos);
+	return str_strip_parens(ans_str, false);
+}
+
+vector<vector<string>> get_global_conj_query_vectors(string conj_str) {
+	vector<vector<string>> res;
+	size_t pos = conj_str.find("<-") + 2;
+	string query_str = conj_str.substr(pos);
+	vector<string> queries = str_split_semicol(query_str);
+	for (auto& s : queries) {
+		vector<string> q = str_strip_parens(s, true);
+		res.push_back(q);
+	}
+	return res;
+}
+
+string front_end() {
+
+	string global_conj_str;
 
 	// print global schema, ask for schema input
-	cout<<"\nGLOBAL SCHEMAS"<<endl;
+	cout << "\nGLOBAL SCHEMAS" << endl;
 	cout << "1. "  << NOMINEE_GLOBAL_SCHEMA << endl;
     cout << "2. "  << MOVIE_GLOBAL_SCHEMA << endl;
     cout << "3. "  << PRINCIPAL_GLOBAL_SCHEMA << endl;
@@ -46,64 +89,45 @@ vector<vector<string> > front_end() {
     cout << "6. "  << FINANCE_GLOBAL_SCHEMA << endl;
     cout << "7. "  << MINORITY_ACTOR_GLOBAL_SCHEMA << endl;
     cout << "8. "  << ACTOR_BY_COUNTRY_GLOBAL_SCHEMA << endl;
-    cout << "9. " << COMMERCIALLY_SUCCESSFUL_MOVIE_GLOBAL_SCHEMA << endl;
+    cout << "9. "  << COMMERCIALLY_SUCCESSFUL_MOVIE_GLOBAL_SCHEMA << endl;
 	cout << "10. " << HIGH_RATING_MOVIE_GLOBAL_SCHEMA << endl;
     cout << "11. " << OSCAR_MOVIE_RATING_GLOBAL_SCHEMA << endl;
-    
-	cout << "\nPlease specify the schemas you wish to include, and press ENTER to finish. E.g. Nominee,Movie,Principal [ENTER]" << endl;
+	cout << "\nPlease input a Conjunctive Query, and press ENTER to finish. E.g. ans(*)<-Movie(imdb_title_id,_,_,_,_,_,_);Principal(imdb_title_id,_,_) [ENTER]" << endl;
+	cin  >> global_conj_str;
 
-	// ask and store global schema
-	vector<string> schema_list;
-	schema_list = listen_and_store("");
-    cout << endl;
-
-	// prompting for parameters
-	cout << "Please enter schema parameters, if not specified, use '_'. E.g. a,b,_,d,_,f [ENTER]" << endl;
-
-	// asking for parameters for each schema
-	for (int i=0; i < schema_list.size(); i++) {
-		string schema_name = schema_list[i];
-		vector<string> para_list;
-		cout << "For "+ schema_name + ":" << endl;
-		para_list = listen_and_store(schema_name);
-		global_conj_queries.push_back(para_list);
-		cout << endl;
-	}
-	
-    return global_conj_queries;
-}
-
-vector<string> get_answer_selection() {
-	cout << "Please select the fields for answer, if return all fields, use '*'. E.g. title,name [ENTER]" << endl;
-	vector<string> res = listen_and_store("");
-	return res;
+    return global_conj_str;
 }
 
 int main()
 {
-	
-    vector<vector<string>> global_conj_queries = front_end();
-	vector<string> answer_selection = get_answer_selection();
-    print_query_vector(global_conj_queries, "global_conj_queries");
+	// Print prompt and Get user input conj query string
+	string global_conj_str = front_end();
+	print_query_str(global_conj_str, "user input conj query str");
 
-	vector<vector<string>> local_conj_queries = unfolding(global_conj_queries);
-    print_query_vector(local_conj_queries, "local_conj_queries");
+	// Break the 1st half of conj query string into answer selection
+	// Break the 2nd half of conj query string into global conj query vectors
+	vector<string> answer_selection = get_answer_selection(global_conj_str);
+	print_str_vector(answer_selection, "answer selection");
+	vector<vector<string>> global_conj_query_vectors = get_global_conj_query_vectors(global_conj_str);
+    print_query_vector(global_conj_query_vectors, "global_conj_query_vectors");
 
-	vector<string> sql_queries = translating(local_conj_queries);
+	exit(0);
+
+	// Unfold the global conj query vectors into local conj query vectors
+	vector<vector<string>> local_conj_query_vectors = unfolding(global_conj_query_vectors);
+    print_query_vector(local_conj_query_vectors, "local_conj_query_vectors");
+
+	// Translate the local conj query vectors into sql queries
+	vector<string> sql_queries = translating(local_conj_query_vectors);
 	print_sql_vector(sql_queries, "sql_queries");
 
-	string sql_query_to_db = join_queries(sql_queries, local_conj_queries, answer_selection);
-	cout << "------------------------------------Testing: Print final sql query" << "------------------------------------" << endl << sql_query_to_db << endl;
+	// Join the sql queries into a final sql query
+	string sql_query_to_db = join_queries(sql_queries, local_conj_query_vectors, answer_selection);
+	print_query_str(sql_query_to_db, "final sql query");
 	
+	// Send the final sql query to backend, Query the DB
 	cout << endl << "Sending SQL query to DB..." << endl;
-
 	query_db(sql_query_to_db);
-	
-
-	//string test1 = "SELECT budget FROM (SELECT movies.imdb_title_id, movies.title, movies.year_film, movies.genre, movies.duration, movies.country, movies.language FROM movies WHERE genre=\"Drama\") t0 INNER JOIN (SELECT title_principals.imdb_title_id, title_principals.imdb_name_id, title_principals.job_category FROM title_principals WHERE job_category=\"actor\" ) t1 ON t0.imdb_title_id = t1.imdb_title_id INNER JOIN (SELECT names.imdb_name_id, names.name, names.height FROM names ) t2 ON t1.imdb_name_id = t2.imdb_name_id INNER JOIN (SELECT oscar_personnel.won, oscar_personnel.place_of_birth, oscar_personnel.date_of_birth, oscar_personnel.race, oscar_personnel.religion, oscar_personnel.sexual_orientation, oscar_personnel.name FROM oscar_personnel WHERE won=\"FALSE\"  AND sexual_orientation=\"Gay\") t3 ON t2.name = t3.name ;";
-	//string test2 = "SELECT movies.title FROM movies INNER JOIN title_principals ON title_principals.imdb_title_id=movies.imdb_title_id INNER JOIN names ON names.imdb_name_id=title_principals.imdb_name_id INNER JOIN oscar_personnel ON oscar_personnel.name=names.name WHERE movies.genre=\"Drama\" AND title_principals.job_category=\"actor\" AND oscar_personnel.sexual_orientation=\"Gay\";";
-	//query_db(test1);
-	
 	
     return 0;
 }
--- utility.h
+++ utility.h
@@ -6,22 +6,34 @@
 
 using namespace std;
 
-void print_query_vector(vector<vector<string>> query_vector, string comment) {
-    cout << "------------------------------------Testing: Print " << comment << "------------------------------------" << endl;
+void print_query_str(const string& query_str, const string& comment) {
+    cout << "\n------------------------------------Testing: Print " << comment << "------------------------------------" << endl;
+    cout << query_str << endl;
+}
+
+void print_str_vector(const vector<string>& str_vec, const string& comment) {
+     cout << "\n------------------------------------Testing: Print " << comment << "------------------------------------" << endl;
+     for (auto& s : str_vec) cout << s << " ";
+     cout << endl;
+}
+
+void print_query_vector(const vector<vector<string>>& query_vector, const string& comment) {
+    cout << "\n------------------------------------Testing: Print " << comment << "------------------------------------" << endl;
     for (int i = 0; i < query_vector.size(); i++){
     	cout << i + 1 << ". ";
     	for (int j = 0; j < query_vector[i].size(); j++){
-            if (j == 0) cout << query_vector[i][j] + " (";
-            else if (j == query_vector[i].size() - 1) cout << query_vector[i][j] + ")";
-    		else cout << query_vector[i][j] + ",";
+            if (j == 0) cout << "[" + query_vector[i][j];
+            else if (j == query_vector[i].size() - 1) cout << query_vector[i][j] + "]";
+    		else cout << query_vector[i][j];
+            if (j != query_vector[i].size() - 1) cout << ",";
     	}
     	cout << endl;
     }
     cout << endl;
 }
 
-void print_sql_vector(vector<string> sql_queries, string comment) {
-    cout << "------------------------------------Testing: Print " << comment << "------------------------------------" << endl;
+void print_sql_vector(const vector<string>& sql_queries, const string& comment) {
+    cout << "\n------------------------------------Testing: Print " << comment << "------------------------------------" << endl;
     for (int i = 0; i < sql_queries.size(); i++){
     	cout << i + 1 << ". ";
     	cout << sql_queries[i] << endl;
